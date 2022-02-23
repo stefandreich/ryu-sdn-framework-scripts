aab79b8558482f019b5f89ea8ec1ec880af3d364
Ștefan Chichișan <74013165+stefandreich@users.noreply.github.com>
2022-02-23 12:03:43 +0200

    Update loadbalancer.py

--- loadbalancer.py
+++ loadbalancer.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2011 Nippon Telegraph and Telephone Corporation.
+# Copyright (C) Ștefan Chichisan.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
