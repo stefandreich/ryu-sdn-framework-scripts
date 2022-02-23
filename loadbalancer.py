339cd26a134d7098f4107765ea283345808bc36d
Ștefan Chichișan <74013165+stefandreich@users.noreply.github.com>
2022-02-23 12:04:12 +0200

    Update loadbalancer.py

--- loadbalancer.py
+++ loadbalancer.py
@@ -1,4 +1,4 @@
-# Copyright (C) Ștefan Chichisan.
+# Copyright (C) 2021 Ștefan Chichișan
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
